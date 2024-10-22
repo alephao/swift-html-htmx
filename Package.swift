b94fd31e44259ebd24a83393e3d1fde2a27ee51e
alephao <7674479+alephao@users.noreply.github.com>
2024-10-21 22:04:57 -0300

    update swift-html to 0.5.0

--- Package.swift
+++ Package.swift
@@ -10,16 +10,16 @@ let package = Package(
     .tvOS(.v13),
   ],
   products: [
-    .library(name: "HtmlHtmx", targets: ["HtmlHtmx"]),
+    .library(name: "HtmlHtmx", targets: ["HtmlHtmx"])
   ],
   dependencies: [
-    .package(url: "https://github.com/pointfreeco/swift-html.git", from: "0.4.0"),
+    .package(url: "https://github.com/pointfreeco/swift-html.git", from: "0.5.0")
   ],
   targets: [
     .target(
       name: "HtmlHtmx",
       dependencies: [
-        .product(name: "Html", package: "swift-html"),
+        .product(name: "Html", package: "swift-html")
       ]
     )
   ]
